5ec0422db34824bbfb71803fd2305b600303e5b0
root <root@CHRIS-MATEBOOK.localdomain>
2023-10-10 11:39:56 +0200

    Initial commit

---
+++ my_test.py
@@ -0,0 +1,11 @@
+from prefect import flow
+from prefect.testing.utilities import prefect_test_harness
+
+@flow
+def my_favorite_flow():
+    return 42
+
+def test_my_favorite_flow():
+  with prefect_test_harness():
+      # run the flow against a temporary testing database
+      assert my_favorite_flow() == 42   
